cd3d2e5d8a07b1bc4ce35c1f53c73dd9de3f324c
Parth Mangtani <impmmmm@gmail.com>
2021-05-28 13:48:13 +0530

    add notification if app updates

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -69,6 +69,9 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
+              alert(
+                "New content is available and will be used when all tabs for this page are closed."
+              );
               console.log(
                 "New content is available and will be used when all " +
                   "tabs for this page are closed. See https://cra.link/PWA."
